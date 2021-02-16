40722815c5ee454e4562767b167647d24b72571b
Mr. Nice <mr.nice@MrNice-MacBook-Pro.local>
2021-02-16 01:24:37 -0800

    initial version

--- main.tf
+++ main.tf
@@ -1,17 +1,102 @@
 terraform {
-  required_version = "= 0.12.20"
+  required_version = "= 0.14.6"
 }
 
-provider "" {
+provider "azurerm" {
   features {}
+}
+
+data "azurerm_image" "custom_image_info" {
+  name = var.custom_image_name
+  resource_group_name = var.custom_image_resource_group_name
 }
 
 data "null_data_source" "common_tags" {
   inputs = {
-    infrastructure = var.infrastructure
+    application_name = var.application_name,
+    cost_center = var.cost_center,
+    department_name = var.department_name,
+    delivery_team_name = var.delivery_team_name,
+    pattern_name = var.pattern_name,
+    resource_owner = var.resource_owner,
   }
 }
 
-module "modules-0-0-0" {
-  source = ""
+resource "azurerm_resource_group" "az_rg" {
+  location = var.location
+  name = var.resource_group_name
+  tags = merge(
+    data.null_data_source.common_tags.outputs,
+    map(
+      "resource_type", "resource group"
+    )
+  )
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  address_space = var.vnet_address_space
+  location = azurerm_resource_group.az_rg.location
+  name = var.vnet_name
+  resource_group_name = azurerm_resource_group.az_rg.name
+  tags = merge(
+    data.null_data_source.common_tags.outputs,
+    map(
+      "resource_type", "virtual network"
+    )
+  )
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = var.subnet_name
+  resource_group_name = azurerm_resource_group.az_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = var.subnet_address_prefixes
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = var.nic_name
+  ip_configuration {
+    name = var.nic_name
+    private_ip_address_allocation = "Dynamic"
+    subnet_id = azurerm_subnet.subnet.id
+  }
+  location = azurerm_resource_group.az_rg.location
+  resource_group_name = azurerm_resource_group.az_rg.name
+}
+
+resource "azurerm_virtual_machine" "az_vm" {
+  name = var.az_vm_name
+  location = azurerm_resource_group.az_rg.location
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  os_profile {
+    admin_username = var.az_vm_username
+    admin_password = var.az_vm_password
+    computer_name = var.az_vm_name
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  resource_group_name = azurerm_resource_group.az_rg.name
+  storage_image_reference {
+    id = azurerm_virtual_machine.az_vm.id
+  }
+  storage_data_disk {
+    name              = var.az_vm_name
+    managed_disk_type = var.managed_disk_type
+    disk_size_gb      = var.storage_data_disk_disk_size_db
+    create_option     = var.storage_os_disk_create_option
+    lun               = var.storage_data_disk_lun
+  }
+  storage_os_disk {
+    create_option = var.storage_os_disk_create_option
+    name = var.az_vm_name
+    managed_disk_type = var.managed_disk_type
+  }
+  tags = merge(
+      data.null_data_source.common_tags.outputs,
+      map(
+        "resource_type", "azure virtual machine"
+      )
+  )
+  vm_size = var.az_vm_size
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,140 @@
-variable "infrastructure" {
+# Data
+variable "custom_image_name" {
+  default     = ""
+  description = "The name of the Image."
   type        = string
+}
+
+variable "custom_image_resource_group_name" {
+  default     = ""
+  description = "The Name of the Resource Group where this Image exists."
+  type        = string
+}
+
+# Resources
+variable "az_vm_name" {
+  default     = ""
+  description = "Specifies the name of the Virtual Machine."
+  type        = string
+}
+
+variable "az_vm_password" {
+  default     = ""
+  description = "The password associated with the local administrator account."
+  type        = string
+}
+
+variable "az_vm_size" {
+  default     = "Standard_D4a_v4"
+  description = "Specifies the size of the Virtual Machine."
+  type        = string
+}
+
+variable "az_vm_username" {
+  default     = ""
+  description = "Specifies the name of the local administrator account."
+  type        = string
+}
+
+variable "location" {
+  default     = "West US 2"
+  description = "The Azure Region where the Resource Group should exist."
+  type        = string
+}
+
+variable "managed_disk_type" {
+  default = "Premium_LRS"
+  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+  type = string
+}
+
+variable "nic_name" {
+  default     = ""
+  description = "The name of the Network Interface."
+  type        = string
+}
+
+variable "resource_group_name" {
+  default     = ""
+  description = "The Name which should be used for this Resource Group."
+  type        = string
+}
+
+variable "storage_data_disk_disk_size_db" {
+  default = "75"
+  description = "Specifies the size of the data disk in gigabytes."
+  type = string
+}
+
+variable "storage_data_disk_lun" {
+  default     = "0"
+  description = "Specifies the logical unit number of the data disk. This needs to be unique within all the Data Disks on the Virtual Machine."
+  type        = string
+}
+
+variable "storage_os_disk_create_option" {
+  default     = "FromImage"
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  default     = ["10.0.1.0/24"]
+  description = "The address prefixes to use for the subnet."
+  type        = list
+}
+
+variable "subnet_name" {
+  default     = ""
+  description = "The name of the subnet."
+  type        = string
+}
+
+variable "vnet_address_space" {
+  default     = ["10.0.0.0/16"]
+  description = "The address space that is used the virtual network."
+  type        = list
+}
+
+variable "vnet_name" {
+  default     = ""
+  description = "The name of the virtual network."
+  type        = string
+}
+
+# Tags
+variable "application_name" {
+  default     = ""
+  description = "Tag name for capturing application name."
+  type        = string
+}
+
+variable "cost_center" {
+  default     = ""
+  description = "Tag name for capturing cost center."
+  type        = string
+}
+
+variable "department_name" {
+  default     = ""
+  description = "Tag name for capturing department name."
+  type        = string
+}
+
+variable "delivery_team_name" {
+  default     = ""
+  description = "Tag name for capturing delivery team name."
+  type        = string
+}
+
+variable "pattern_name" {
   default     = ""
   description = "Tag name for capturing infrastructure pattern name."
+  type        = string
+}
+
+variable "resource_owner" {
+  default     = ""
+  description = "Tag name for capturing resource owner."
+  type        = string
 }
